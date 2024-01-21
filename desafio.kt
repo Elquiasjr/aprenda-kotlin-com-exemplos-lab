826132f764a89d2c920ef8e76590e756ce12b172
Elquias Junior <83426006+Elquiasjr@users.noreply.github.com>
2024-01-21 03:57:27 -0300

    Desafio Finalizado

--- desafio.kt
+++ desafio.kt
@@ -19,9 +19,9 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>, 
 
 fun main() {
     val conteudos = mutableListOf<ConteudoEducacional>()
-    conteudos.add(ConteudoEducacional("Introducao ao C#", 30));
-    conteudos.add(ConteudoEducacional("Funcoes com C#", 40));
-    conteudos.add(ConteudoEducacional("Exceptions com C#", 50));
+    conteudos.add(ConteudoEducacional("Introducao ao C#", 30))
+    conteudos.add(ConteudoEducacional("Funcoes com C#", 40))
+    conteudos.add(ConteudoEducacional("Exceptions com C#", 50))
     
     val cSharp = Formacao("Curso Completo de C#", conteudos, Nivel.INTERMEDIARIO)
     
@@ -31,8 +31,26 @@ fun main() {
     alunos.add(Usuario("Gabriel"))
     alunos.add(Usuario("Jonathan"))
     
+    println("Alunos: $alunos")
+    
     cSharp.matricular(*alunos.toTypedArray())
     
-    println(cSharp)
-    println(cSharp.inscritos)
+    println("Informacoes da formacao:\n$cSharp")
+    println("Alunos cadastrados na formacao: ${cSharp.inscritos}")
+    
+    println()
+    
+    val conteudos1 = mutableListOf<ConteudoEducacional>()
+    conteudos1.add(ConteudoEducacional("Introducao a Probabilidade", 30))
+    conteudos1.add(ConteudoEducacional("Fundamentos da Estatística", 30))
+    
+    val probEEstatistica = Formacao("Curso de Probabilidade e Estatística", conteudos1, Nivel.BASICO)
+    
+    alunos.removeAt(0)
+    println("Alunos: $alunos")
+    
+    probEEstatistica.matricular(*alunos.toTypedArray())
+    
+    println("Informacoes da formacao:\n$probEEstatistica")
+    println("Alunos cadastrados na formacao: ${probEEstatistica.inscritos}")
 }
